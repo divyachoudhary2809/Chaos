8558e66d0dda272ea9248c2b71b5c91ed7d12159
Divya Choudhary <divya.choudhary2809@gmail.com>
2023-10-24 18:37:11 +0100

    Update run_oxidative.py

--- Oxidative_stress/run_oxidative.py
+++ Oxidative_stress/run_oxidative.py
@@ -15,7 +15,6 @@ g0 = 0.042;
 concentration=250. ### treatment concentration in uM  
 
 
-
 ### if you want to choose randomised initial conditions
 ## for having a randomised structure of 'N' = number of trenches.  just supply N in the line below
 numberoftrench = 3
@@ -24,11 +23,15 @@ for ntrench in range(numberoftrench):
     trenches[ntrench] =[] ;  
     len_=0
     for i in range(0, random.randint(3,8)):
-        if len_ < trenchLength:
+        if len_ < TrenchLength:
             lenadd= random.randint(2010, 3990)/1000.
             trenches[ntrench] .append(lenadd )
             len_+=lenadd#'''
             
+            
+## for having initial structure of your choice you can use uncomment the line below and add the values you like: 
+#trenches=[[2.241, 2.342, 3.043, 2.244,3.067,3.241]]  ### structure of cells at the start
+        
              
         
 df = run.main_run( trenchLength, g0, trenches, duration, concentration,time_of_treat) 
